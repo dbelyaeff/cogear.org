1cde074c75a9673dc08521903c492c08e9b3a340
CuamckuyKot <admin@cogear.ru>
2011-08-30 18:11:35 +0400

    Fix flash messages bug.

--- engine/Messages/Gear.php
+++ engine/Messages/Gear.php
@@ -25,6 +25,7 @@ class Messages_Gear extends Gear {
     const INFO = 0;
     const DIALOG = 1;
     const AJAX = 2;
+
     /**
      * Init
      */
@@ -45,12 +46,12 @@ class Messages_Gear extends Gear {
         $tpl = new Template("Messages.notification_jgrowl");
         $tpl->content = $content;
         $tpl->class = $class;
-	    $tpl->type = $type ? $type : self::INFO;
+        $tpl->type = $type ? $type : self::INFO;
 //	    if($class=="error") $tpl->sticky = 'true';
         prepend('content', $tpl->render());
     }
 
-	/**
+    /**
      * Show message
      *
      * @param string $content
@@ -63,31 +64,33 @@ class Messages_Gear extends Gear {
         $tpl->title = $title;
         $tpl->content = $content;
         $tpl->class = $class;
-	    $tpl->type = $type ? $type : self::INFO;
+        $tpl->type = $type ? $type : self::INFO;
         prepend('content', $tpl->render());
     }
 
-	/*
-	 * Show dialog window
-	 */
-	public function show_dialog_close($content = NULL, $title = NULL, $class='info') {
-		$tpl = new Template("Messages.dialog_close");
-		$tpl->title = $title;
-        $tpl->content = $content;
-        $tpl->class = $class;
-		prepend('content', $tpl->render());
-	}
+    /*
+     * Show dialog window
+     */
 
-	/*
-	 * Show dialog window with confirmation
-	 */
-	public function show_dialog_confirmation($content = NULL, $title = NULL, $class='info') {
-		$tpl = new Template("Messages.dialog_confirmation");
-		$tpl->title = $title;
+    public function show_dialog_close($content = NULL, $title = NULL, $class='info') {
+        $tpl = new Template("Messages.dialog_close");
+        $tpl->title = $title;
         $tpl->content = $content;
         $tpl->class = $class;
-		prepend('content', $tpl->render());
-	}
+        prepend('content', $tpl->render());
+    }
+
+    /*
+     * Show dialog window with confirmation
+     */
+
+    public function show_dialog_confirmation($content = NULL, $title = NULL, $class='info') {
+        $tpl = new Template("Messages.dialog_confirmation");
+        $tpl->title = $title;
+        $tpl->content = $content;
+        $tpl->class = $class;
+        prepend('content', $tpl->render());
+    }
 
     /**
      * Set flash message
@@ -109,7 +112,7 @@ class Messages_Gear extends Gear {
     public function finish() {
         if ($this->session->messages) {
             foreach ($this->session->messages as $offset => $data) {
-                call_user_func_array(array($this, 'show'), $data);
+                call_user_func_array(array($this, 'show_message'), $data);
             }
             $this->session->destroy('messages');
         }
@@ -119,10 +122,10 @@ class Messages_Gear extends Gear {
         inline_js("$(document).ready(function(){window.Messenger.render()});");
     }
 
-	public function set_template($template)
-	{
-		if($template!="") $this->template = $template;
-	}
+    public function set_template($template) {
+        if ($template != "")
+            $this->template = $template;
+    }
 
 }
 
@@ -133,8 +136,8 @@ class Messages_Gear extends Gear {
  * @param string $title
  */
 function success($content=NULL, $title=NULL) {
-	$class='success';
-	$content OR $content = t('Operation is successfully completed.');
+    $class = 'success';
+    $content OR $content = t('Operation is successfully completed.');
     cogear()->messages->show_message($content, $title, $class);
 }
 
@@ -183,8 +186,8 @@ function flash_info($content=NULL, $title=NULL, $class='info') {
  * @param string $class 
  */
 function error($content=NULL, $title=NULL) {
-	$class='error';
-	$content OR $content = t('Operation failed.');
+    $class = 'error';
+    $content OR $content = t('Operation failed.');
     cogear()->messages->show_message($content, $title, $class);
 }
 
@@ -209,7 +212,7 @@ function flash_error($content=NULL, $title=NULL, $class='error') {
  * @param string $class
  */
 function dialog_close($title=NULL, $content=NULL, $class='info') {
-	cogear()->messages->show_dialog_close($content, $title, $class);
+    cogear()->messages->show_dialog_close($content, $title, $class);
 }
 
 /**
@@ -220,7 +223,7 @@ function dialog_close($title=NULL, $content=NULL, $class='info') {
  * @param string $class
  */
 function dialog_confirmation($title=NULL, $content=NULL, $class='info') {
-	cogear()->messages->show_dialog_confirmation($content, $title, $class);
+    cogear()->messages->show_dialog_confirmation($content, $title, $class);
 }
 
 /**
@@ -231,6 +234,6 @@ function dialog_confirmation($title=NULL, $content=NULL, $class='info') {
  * @return void
  */
 function notify($content=NULL, $class="info") {
-	$content OR $content = t('Please pay attetion to this notification.');
+    $content OR $content = t('Please pay attetion to this notification.');
     cogear()->messages->show_notification($content, $class);
 }
