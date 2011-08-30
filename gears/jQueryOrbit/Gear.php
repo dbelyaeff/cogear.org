07066814e87d7ff51e65b82cb2a5f46635f8b6d6
CuamckuyKot <admin@cogear.ru>
2011-08-30 19:46:40 +0400

    Introduce jQueryOrbit slider

---
+++ gears/jQueryOrbit/Gear.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * jQuery Orbit
+ *
+ * @author		Dmitriy Belyaev <admin@cogear.ru>
+ * @copyright		Copyright (c) 2011, Dmitriy Belyaev
+ * @license		http://cogear.ru/license.html
+ * @link		http://cogear.ru
+ * @package		Core
+ * @subpackage
+ * @version		$Id$
+ */
+class jQueryOrbit_Gear extends Gear {
+
+    protected $name = 'jQuery Orbit';
+    protected $description = 'Add awesome jQuery Orbit';
+    protected $package = 'jQuery';
+    protected $order = -100;
+
+}
